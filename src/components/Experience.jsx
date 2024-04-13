029feeb4a58a4a2c72fd9fc46aa799567965a4da
Vishalgv3 <vishalcanada03@gmail.com>
2024-04-13 08:03:33 -0300

    added all the necessary models and licences

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,11 @@
 import { OrbitControls } from "@react-three/drei";
+import { Vishal } from "./Avatars/Vishal";
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
+      <Vishal />
       <mesh>
         <boxGeometry />
         <meshNormalMaterial />
