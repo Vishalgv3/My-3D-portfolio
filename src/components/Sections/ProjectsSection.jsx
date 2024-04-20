214ac5ef773335ba3ed07c5554c35af4c0747989
Vishalgv3 <vishalcanada03@gmail.com>
2024-04-20 11:30:07 -0300

    added fontawesome icons in projects section

--- src/components/Sections/ProjectsSection.jsx
+++ src/components/Sections/ProjectsSection.jsx
@@ -3,6 +3,9 @@ import { Section } from "./Section";
 import { currentProjectAtom } from "../Other3D/Project";
 import { projects } from "../../data/projectsData";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquareCaretRight, faSquareCaretLeft } from '@fortawesome/free-solid-svg-icons'
+
 export const ProjectsSection = () => {
 
     const [currentProject, setCurrentProject] = useAtom(currentProjectAtom);
@@ -18,9 +21,9 @@ export const ProjectsSection = () => {
     return (
         <Section>
             <div className="flex w-full h-full gap-8 items-center justify-center text-white">
-                <button className="hover:text-indigo-600 transition-colors text-sm sm:text-base" onClick={previousProject}>Previous</button>
+                <button className="hover:text-indigo-600 transition-colors text-sm sm:text-base" onClick={previousProject}><FontAwesomeIcon icon={faSquareCaretLeft} className="mr-1" />Previous</button>
                 <h2 className="text-3xl md:text-5xl font-bold">Projects</h2>
-                <button className="hover:text-indigo-600 transition-colors text-sm sm:text-base" onClick={nextProject}>Next</button>
+                <button className="hover:text-indigo-600 transition-colors text-sm sm:text-base" onClick={nextProject}>Next<FontAwesomeIcon icon={faSquareCaretRight} className="ml-1" /></button>
             </div>
         </Section>
     )
