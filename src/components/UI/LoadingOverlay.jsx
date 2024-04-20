4db11654fa6a12f79a831e5678257e5a01e8057e
Vishalgv3 <vishalcanada03@gmail.com>
2024-04-20 12:25:19 -0300

    changed the opacity back 0 for when everything is loaded

--- src/components/UI/LoadingOverlay.jsx
+++ src/components/UI/LoadingOverlay.jsx
@@ -19,7 +19,7 @@ export const LoadingOverlay = (props) => {
         <div
             className={`fixed top-0 left-0 w-full h-full z-50 transition-opacity duration-1000 pointer-events-none
   flex items-center justify-center bg-indigo-50 
-  ${started ? "opacity-100" : "opacity-100"}`}
+  ${started ? "opacity-0" : "opacity-100"}`}
         >
             <div className="text-4xl md:text-9xl font-bold text-indigo-900 relative">
                 <div
